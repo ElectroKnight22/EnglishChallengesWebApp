9745794ef178967ab4ab7d9afa1716adb44a76a0
eknightt <trashcan99999999@gmail.com>
2023-03-07 15:43:38 -0800

    Added auth0.

--- Program.cs
+++ Program.cs
@@ -10,9 +10,9 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.
 
 builder.Services.AddOidcAuthentication(options =>
 {
-    // Configure your authentication provider options here.
-    // For more information, see https://aka.ms/blazor-standalone-auth
-    builder.Configuration.Bind("Local", options.ProviderOptions);
+    builder.Configuration.Bind("Auth0", options.ProviderOptions);
+    options.ProviderOptions.ResponseType = "code";
+    options.ProviderOptions.DefaultScopes.Add("offline_access");
 });
 
 await builder.Build().RunAsync();
