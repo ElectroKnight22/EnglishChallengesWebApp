9fe6f0b0682df6709d17150821f5881ba905e049
eknightt <trashcan99999999@gmail.com>
2023-03-07 16:10:58 -0800

    Added auth better.

--- Program.cs
+++ Program.cs
@@ -11,8 +11,9 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.
 builder.Services.AddOidcAuthentication(options =>
 {
     // Configure your authentication provider options here.
-    // For more information, see https://aka.ms/blazor-standalone-auth
-    builder.Configuration.Bind("Local", options.ProviderOptions);
+
+    builder.Configuration.Bind("Auth0", options.ProviderOptions);
+    options.ProviderOptions.ResponseType = "code";
 });
 
 await builder.Build().RunAsync();
