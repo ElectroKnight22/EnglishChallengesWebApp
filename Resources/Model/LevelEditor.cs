57e086766a5fc60c8e04a229a0e95f17b673ab03
eknightt <trashcan99999999@gmail.com>
2023-03-11 17:45:13 -0800

    Added a level selector page.

---
+++ Resources/Model/LevelEditor.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace EnglishChallengesWebApp.Resources.Model
+{
+    public class LevelEditor : ComponentBase, ILevelEditor
+    {
+        public int LevelNumber => throw new NotImplementedException();
+
+        public string LevelType => throw new NotImplementedException();
+
+        public void AddEntry(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EditEntries(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetQuestionSetFromFile()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InitializeEntries()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LoadEntries(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveEntries(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Resources/Model/QuestionSet.cs
+++ Resources/Model/QuestionSet.cs
@@ -1,32 +1,52 @@
-﻿public class QuestionSet
+﻿using System.Text.Json;
+
+public class QuestionSet
 {
-    public string Title { get; set; }
-    public string LastModifiedDate { get; set; }
-    public int LevelNumber { get; set; }
+    public int Id { get; set; }
+    public string Name { get; set; }
     public string LevelType { get; set; }
+    public int LevelNumber { get; set; }
+    public string LastModifiedDate { get; set; }
+    public bool IsPublished { get; set; } = false;
+
     public HashSet<Question> Questions { get; set; }
 
 
-    public QuestionSet(string title, string date, int num, string type)
+    //public QuestionSet(string title, string date, int num, string type)
+    //{
+    //    this.Name = title;
+    //    this.LastModifiedDate = date;
+    //    this.LevelNumber = num;
+    //    this.LevelType = type;
+    //    Questions = new();
+    //}
+    //note: Supabase implementation does not allow constructors with parameters.
+
+    private JsonSerializerOptions jsonOptions = new JsonSerializerOptions
     {
-        this.Title = title;
-        this.LastModifiedDate = date;
-        this.LevelNumber = num;
-        this.LevelType = type;
-        Questions = new();
+        WriteIndented = true
+    };
+
+    public void ToJson()
+    {
+        string json = JsonSerializer.Serialize(Questions, jsonOptions);
     }
+
 
     public override bool Equals(object obj)
     {
         var qSet = obj as QuestionSet;
         return qSet != null &&
-               Title == qSet.Title &&
+               Id == qSet.Id &&
+               Name == qSet.Name &&
                LevelNumber == qSet.LevelNumber &&
                LevelType == qSet.LevelType;
     }
 
     public override int GetHashCode()
     {
-        return HashCode.Combine(Title, LevelNumber, LevelType);
+        return HashCode.Combine(Id, Name, LevelNumber, LevelType);
     }
 }
+
+
